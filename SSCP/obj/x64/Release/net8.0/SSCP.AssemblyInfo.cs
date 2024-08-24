f459e9ec15486056d4655384066883ef7f4f52e1
Gabriele Bologna <bolognagabriele2003@gmail.com>
2024-08-24 08:00:19 +0200

    Changes

--- SSCP/obj/x64/Release/net8.0/SSCP.AssemblyInfo.cs
+++ SSCP/obj/x64/Release/net8.0/SSCP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SSCP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f08d0847d49f35076349aecd37f3dc0ea87c6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c94663cbde9f283758d4ba3337b082d62db0bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SSCP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SSCP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
